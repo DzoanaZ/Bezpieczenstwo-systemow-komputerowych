296725b787f92a9414461ebde8c3564045c522f3
dominik <dgolob@gmail.com>
2018-12-01 12:19:30 +0100

    bezpieczny zrobiony

--- php_bezpieczny/frontend/views/site/login.php
+++ php_bezpieczny/frontend/views/site/login.php
@@ -11,26 +11,22 @@ $this->title = 'Login';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
+    <h1>Logowanie</h1>
 
-    <p>Please fill out the following fields to login:</p>
+    <p>Zaloguj się:</p>
 
     <div class="row">
         <div class="col-lg-5">
             <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
 
-                <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+                <?= $form->field($model, 'username')->textInput(['autofocus' => true] )->label('Login') ?>
 
-                <?= $form->field($model, 'password')->passwordInput() ?>
+                <?= $form->field($model, 'password')->passwordInput()->label('Hasło') ?>
 
-                <?= $form->field($model, 'rememberMe')->checkbox() ?>
-
-                <div style="color:#999;margin:1em 0">
-                    If you forgot your password you can <?= Html::a('reset it', ['site/request-password-reset']) ?>.
-                </div>
+                <?= $form->field($model, 'rememberMe')->checkbox()->checkbox(['label' => 'Zapamiętaj mnie']) ?>
 
                 <div class="form-group">
-                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+                    <?= Html::submitButton('Zaloguj', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
                 </div>
 
             <?php ActiveForm::end(); ?>
